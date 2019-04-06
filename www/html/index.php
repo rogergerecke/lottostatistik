33221f1d14dc657ef67d09a52a1c59b82644c940
rogergerecke <roger.gerecke@gmail.com>
2019-04-06 22:38:40 +0200

    update

---
+++ www/html/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrry
+ * Date: 23.03.19
+ * Time: 10:56
+ */
+
+//echo phpinfo();
+echo $_SERVER['DOCUMENT_ROOT'];
+require_once "../config/bootstrap.php";
